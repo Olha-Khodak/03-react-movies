887f9c20b79514f73cadb97ee27378505db4c685
Olha_Khodak <olgapolina261111@ukr.net>
2025-11-08 21:30:57 +0200

    add structure and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,1 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App/App";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
